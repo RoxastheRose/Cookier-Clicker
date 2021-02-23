50f05df14934482eeebc39ec81c3f2f9d046ef39
Lane <lanejm1998@gmail.com>
2021-02-22 18:40:28 -0700

    Basic Game Scene + Touch
    Adds in a very basic looking game screen as well as a script to add objects at the touch point of the player.

---
+++ Assets/Scripts/TouchPoint.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchPoint : MonoBehaviour
+{
+    [SerializeField] public GameObject m_touchTemplate;
+    
+    void Update()
+    {
+        Vector2 touchPos;
+        bool aTouch = false;
+        if (Application.platform != RuntimePlatform.Android)
+        {
+            aTouch = Input.GetMouseButtonUp(0);
+            touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        } else {
+            aTouch = Input.touchCount > 0;
+            touchPos = Input.GetTouch(0).position;
+        }
+
+        if (aTouch) CreateTouch(touchPos);
+    }
+
+    void CreateTouch(Vector2 position)
+    {
+        Debug.Log("X: " + position.x + " / Y: " + position.y);
+        GameObject toucher = Instantiate(m_touchTemplate);
+        toucher.transform.position = position;
+    }
+}
