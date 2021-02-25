1f1ee58dfb7123eb63ce018ea1dc374b81610e92
Lane <lanejm1998@gmail.com>
2021-02-24 18:05:50 -0700

    Player Stats & Shop
    Fully functional (currently) player stats as well as a function, unimplemented shop script.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public int m_gold = 0;
+    public int m_bank = 100;
+
+    public int m_tapLevel = 1;
+    public int m_baseTapPoints = 1;
+
+    public int m_bugLevel = 1;
+    public float m_bugBonus = 1.0f;
+
+
+    /// <summary>
+    /// Increases the player's gold up to their current maximum allowed.
+    /// Will not deal with a negative value.
+    /// </summary>
+    /// <param name="gold">The amount of gold to receive.</param>
+    /// <returns>True if no excess gold, false if any excess gold.</returns>
+    public bool ReceiveGold(int gold)
+    {
+        bool result = gold < 0;
+        
+        if (!result)
+        {
+            m_gold += gold;
+            result = m_gold <= m_bank;
+            if (!result) m_gold = m_bank;
+        }
+
+        return result;
+    }
+}
---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    /* Power Index
+     * 0 >> Bank // Cost = Bank * 0.75 || Increase = Bank + (Bank / 10)^2
+     * 1 >> Tap // Cost = (Tap Level + 4)^2 || Increase = Tap + 2
+     * 2 >> Bug // Cost = (Tap Level + 4)^2 || Increase = Bug + 0.5
+    */
+
+    public static PlayerStats m_workingPlayer;
+    
+    // True if able to purchase, false if not.
+    public bool PurchasePowerUp(int powerIndex)
+    {
+        bool result = false;
+
+        switch (powerIndex)
+        {
+            case 0:
+                result = BankUp();
+                break;
+            case 1:
+                result = TapUp();
+                break;
+            case 2:
+                result = BugUp();
+                break;
+            default:
+                break;
+        }
+
+        return result;
+    }
+
+    private bool BankUp()
+    {
+        bool result = false;
+        int cost = (int)(m_workingPlayer.m_bank * 0.75f);
+        if (cost <= m_workingPlayer.m_gold)
+        {
+            result = true;
+            m_workingPlayer.m_bank += (m_workingPlayer.m_bank / 10)^2;
+            m_workingPlayer.m_gold -= cost;
+        }
+        return result;
+    }
+
+    private bool TapUp()
+    { 
+        bool result = false;
+        int cost = (m_workingPlayer.m_tapLevel + 4)^2;
+        if (cost <= m_workingPlayer.m_gold)
+        {
+            result = true;
+            m_workingPlayer.m_baseTapPoints += 2;
+            m_workingPlayer.m_gold -= cost;
+            m_workingPlayer.m_tapLevel++;
+        }
+        return result;
+    }
+
+    private bool BugUp()
+    {
+        bool result = false;
+        int cost = (m_workingPlayer.m_tapLevel + 4) ^ 2;
+        if (cost <= m_workingPlayer.m_gold)
+        {
+            result = true;
+            m_workingPlayer.m_bugBonus += 0.5f;
+            m_workingPlayer.m_gold -= cost;
+            m_workingPlayer.m_bugLevel++;
+        }
+        return result;
+    }
+}
