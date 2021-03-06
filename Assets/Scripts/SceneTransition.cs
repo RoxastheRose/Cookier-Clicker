84754f5c4612ca2bbf79651da9c78ed6e9a7c725
Alexjandropiedra <alexrock9657@gmail.com>
2021-03-06 15:08:04 -0700

    Added New features to the game scene

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -5,12 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public GameObject player;
     public void GoToGameScene()
     {
         SceneManager.LoadScene("GameScene");
     }
     public void GoToTitleScene()
     {
-        SceneManager.LoadScene("GameScreen");
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void PauseScene()
+    {
+        pauseMenu.SetActive(true);
+        player.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void UnPauseScene()
+    {
+        pauseMenu.SetActive(false);
+        player.SetActive(true);
+        Time.timeScale = 1f;
     }
 }
