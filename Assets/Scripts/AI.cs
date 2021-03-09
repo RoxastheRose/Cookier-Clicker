fb8d43df5eb297a28e1aa0d8c743e74c45a299b4
Alexjandropiedra <alexrock9657@gmail.com>
2021-03-08 18:47:37 -0700

    Updated Game Scene

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -6,20 +6,35 @@ using UnityEngine;
 public class AI : MonoBehaviour
 {
     public Vector3 originalPosition;
+    public bool clicked;
     // Start is called before the first frame update
     void Start()
     {
+        clicked = true;
         originalPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(0f, Time.deltaTime, 0f);
+        if (clicked)
+        {
+            transform.position += new Vector3(0f, Time.deltaTime, 0f);
+        }
+        else
+        {
+            transform.position = Vector3.zero;
+        }
+        
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        clicked = false;
+    }
+    private void OnMouseDown()
+    {
+        clicked = true;
         transform.position = originalPosition;
     }
 }
