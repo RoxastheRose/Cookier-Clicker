019d3d66adefa29b48f667c42d74921ab4122ec5
Alexjandropiedra <alexrock9657@gmail.com>
2021-03-10 10:09:00 -0700

    Added a scrolling background

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public Renderer renderer;
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+    }
+    void Update()
+    {
+        renderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0.0f);
+    }
+}
